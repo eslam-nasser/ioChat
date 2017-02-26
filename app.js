c289bcfd10c468ba329a43bae2d66fd5429be667
eslamNasser <eslamnasser@hmaserv.net>
2017-02-26 20:33:09 +0200

    heroku configs

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var users 			= []
 var connections	= []
 var messages 		= []
 
-server.listen(3000, function(){
+server.listen(process.env.PORT || 3000, function(){
 	console.log('Server is running!')
 })
 
